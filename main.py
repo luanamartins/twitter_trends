825d76d0723002ec449805574b064ded9b9bfeb5
Luana Martins <luana.cin@gmail.com>
2020-03-29 13:03:26 -0300

    Refactor

--- main.py
+++ main.py
@@ -19,21 +19,37 @@ api = twitter.Api(consumer_key=s.consumer_key,
                   access_token_key=s.access_token_key,
                   access_token_secret=s.access_token_secret)
 
-trends = api.GetTrendsWoeid(woeid)
-first = trends[0]
-query = first.url
-prefix_size = len('https://twitter.com/search')
-query = query[prefix_size:]
-query = query + '&count=2'
-print(query)
-query_example = "q=twitter%20&result_type=recent&since=2014-07-19&count=100"
-results = api.GetSearch(raw_query=query)
 
-bom = botometer.Botometer(wait_on_ratelimit=True, rapidapi_key=s.rapidapi_key, **twitter_app_auth)
-account = results[0]
-for account in results:
-    name = '@' + account.user.screen_name
-    bom_score = bom.check_account(name)
-    score = bom_score["display_scores"]["universal"]
-    print(name + " " + str(score))
 
+def query_trends():
+    trends = api.GetTrendsWoeid(woeid)
+    prefix_size = len('https://twitter.com/search')
+    # query_example = 'q=twitter%20&result_type=recent&since=2014-07-19&count=100'
+
+    results = []
+    for trend in trends:
+        query = trend.url
+        query = query[prefix_size:]
+        query = query + '&result_type=recent&count=10'
+        print(query)
+        tweets = api.GetSearch(raw_query=query)
+        accounts = []
+        for tweet in tweets:
+            accounts.append(tweet.user.screen_name)
+        results.append(accounts)
+
+    return results
+
+
+
+def bot_classifier(results, twitter_app_auth):
+    bom = botometer.Botometer(wait_on_ratelimit=True, rapidapi_key=s.rapidapi_key, **twitter_app_auth)
+    for account in results:
+        name = '@' + account.user.screen_name
+        bom_score = bom.check_account(name)
+        score = bom_score["display_scores"]["universal"]
+        print(name + ' ' + str(score))
+
+
+results = query_trends()
+print(results)
